be7df711bb709f371aec83b84660b2f06ea0b678
shekhar maheswari <shekharmaheswari85@gmail.com>
2020-10-16 14:12:09 +0600

    Postgres SQL error has been handled

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-
-from db import db
+import os
 from blacklist import BLACKLIST
 from resources.user import UserRegister, User, UserLogin, TokenRefresh, UserLogout
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///data.db"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["JWT_BLACKLIST_ENABLED"] = True  # enable blacklist feature
@@ -17,7 +18,7 @@ app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = [
     "access",
     "refresh",
 ]  # allow blacklisting for access and refresh tokens
-app.secret_key = "jose"  # could do app.config['JWT_SECRET_KEY'] if we prefer
+app.secret_key = "shekhar"  # could do app.config['JWT_SECRET_KEY'] if we prefer
 api = Api(app)
 
 
