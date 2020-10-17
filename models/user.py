428ee630c73ce9477af0d13873923a8cd4bc531c
shekhar maheswari <shekharmaheswari85@gmail.com>
2020-10-17 14:27:07 +0600

    Refactored and classmethod for every methods has been defined

--- models/user.py
+++ models/user.py
@@ -1,35 +1,38 @@
 from db import db
+from typing import Dict, Union, List
+
+UserModelJSON = Dict[str, Union[int, str]]
 
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
+    username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(80))
 
-    def __init__(self, username, password):
+    def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
 
-    def json(self):
+    def json(self) -> UserModelJSON:
         return {
-            'id': self.id,
-            'username': self.username
+            "id": self.id,
+            "username": self.username,
         }
 
-    def save_to_db(self):
+    def save_to_db(self) -> None:
         db.session.add(self)
         db.session.commit()
-    
-    def delete_from_db(self):
+
+    def delete_from_db(self) -> None:
         db.session.delete(self)
         db.session.commit()
 
     @classmethod
-    def find_by_username(cls, username):
+    def find_by_username(cls, username: str) -> "UserModel":
         return cls.query.filter_by(username=username).first()
 
     @classmethod
-    def find_by_id(cls, _id):
+    def find_by_id(cls, _id: int) -> List["UserModel"]:
         return cls.query.filter_by(id=_id).first()
